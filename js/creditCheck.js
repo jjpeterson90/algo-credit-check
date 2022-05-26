607184dc744cb648cc062c92c3d5d7f1d45afb12
Justin <jjpeterson90@hotmail.com>
2022-05-26 15:11:21 -0500

    complete

--- js/creditCheck.js
+++ js/creditCheck.js
@@ -1,2 +1,24 @@
+//exports.creditCheck = function(str) {
 exports.creditCheck = function(str) {
+
+    let checkSum = 0;
+    let valid = "The number is valid!";
+    let invalid = "The number is invalid!";
+    newStr = str.split('');
+
+    for (let i = str.length-2; i >= 0; i -= 2) {
+        newStr[i] *= 2;
+        if (newStr[i] > 9) {
+            newStr[i] = newStr[i].toString().split('');
+            newStr[i] = (newStr[i][0]*1) + (newStr[i][1]*1)
+        }
+    }
+
+    newStr.forEach(elem => {
+        checkSum += elem * 1;
+    })
+
+    return checkSum % 10 === 0 ? valid : invalid
 }
+
+//console.log(creditCheck('5541808923795240'))
